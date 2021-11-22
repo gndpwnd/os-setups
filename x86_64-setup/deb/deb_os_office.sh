f256c354d246ad3a4a01f2c15f48fbc8e9f79ced
Gatlin Nelson <88689368+GatlinNelson@users.noreply.github.com>
2021-11-21 15:35:42 -0900

    Update deb_os_office.sh

--- x86_64-setup/deb/deb_os_office.sh
+++ x86_64-setup/deb/deb_os_office.sh
@@ -1,19 +1,19 @@
 #!/bin/bash
+mkdir /opt/server
 
+sudo apt update && sudo apt install -fy curl wget python3 python3-pip
 curl https://sh.rustup.rs -sSf | sh
 wget https://raw.githubusercontent.com/platformio/platformio-core-installer/master/get-platformio.py -O get-platformio.py
 python3 get-platformio.py
 sudo apt update && sudo apt upgrade
 sudo apt install -fy snap build-essential libelf-dev linux-headers-`uname -r` bc dkms apt-transport-https xclip terminator thunderbird libreoffice libgconf-2-4 libappindicator1 libc++1 git docker iperf3 speedtest-cli python3 python3-pip rust go 
-sudo snap install ao spotify obs-studio vlc discord cherrytree
+sudo snap install ao spotify obs-studio vlc discord cherrytree cura-slicer
 sudo snap install blender --channel=2.79/stable --classic
-sudo snap install cura-slicer
-
-mkdir /opt/server
+sudo snap install sublime-text --classic
   
 #networking
-echo "nameserver 1.1.1.1" > /etc/resolv.conf
-echo "nameserver 1.0.0.1" >> /etc/resolv.conf
+sudo echo "nameserver 1.1.1.1" > /etc/resolv.conf
+sudo echo "nameserver 1.0.0.1" >> /etc/resolv.conf
 
   
 #wireless drivers
@@ -22,17 +22,8 @@ echo "nameserver 1.0.0.1" >> /etc/resolv.conf
 cd /opt
 git clone https://github.com/aircrack-ng/rtl8812au.git
 cd rtl8812au/
-make && make install
+sudo make && sudo make install
 cd /opt/
-
-                    #office
-
-#subl
-wget -qO - https://download.sublimetext.com/sublimehq-pub.gpg | sudo apt-key add -
-#sudo apt-get install apt-transport-https
-echo "deb https://download.sublimetext.com/ apt/stable/" | sudo tee /etc/apt/sources.list.d/sublime-text.list
-sudo apt-get update
-sudo apt-get install sublime-text
 
 sudo apt --fix-broken install
 

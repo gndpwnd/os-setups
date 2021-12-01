3915104c8068378db400fa6bda00734359539137
gndpwnd <88689368+gndpwnd@users.noreply.github.com>
2021-11-30 18:34:56 -0900

    Update deb_os_office.sh

--- x86_64-setup/deb/deb_os_office.sh
+++ x86_64-setup/deb/deb_os_office.sh
@@ -1,16 +1,23 @@
 #!/bin/bash
 mkdir /opt/server
 
+#networking
+sudo echo "nameserver 1.1.1.1" > /etc/resolv.conf
+sudo echo "nameserver 1.0.0.1" >> /etc/resolv.conf
+
 sudo apt update && sudo apt install -fy curl wget python3 python3-pip
 curl https://sh.rustup.rs -sSf | sh
 wget https://raw.githubusercontent.com/platformio/platformio-core-installer/master/get-platformio.py -O get-platformio.py
 python3 get-platformio.py
-sudo apt update && sudo apt upgrade
+rm -rf get-platformio.py
+sudo apt update && sudo apt upgrade -y
 sudo apt install -fy vim snap gparted build-essential libelf-dev linux-headers-`uname -r` bc dkms \ 
-                     apt-transport-https xclip terminator thunderbird libreoffice libgconf-2-4 libappindicator1 libc++1 \
-                     git docker.io iperf3 speedtest-cli golang 
+                     apt-transport-https xclip terminator thunderbird libreoffice \
+                     libgconf-2-4 libappindicator1 libc++1 \
+                     git docker.io iperf3 speedtest-cli golang \
                      
-sudo snap install ao spotify obs-studio vlc discord cherrytree cura-slicer rpi-imager
+                     
+sudo snap install ao spotify vlc discord cherrytree cura-slicer rpi-imager
 sudo snap install blender --channel=2.79/stable --classic
 sudo snap install sublime-text --classic
 sudo snap install code --classic
@@ -18,11 +25,16 @@ sudo snap install code --classic
 sudo add-apt-repository ppa:maarten-baert/simplescreenrecorder
 sudo apt-get update
 sudo apt-get install simplescreenrecorder
-  
-#networking
-sudo echo "nameserver 1.1.1.1" > /etc/resolv.conf
-sudo echo "nameserver 1.0.0.1" >> /etc/resolv.conf
 
+wget http://ftp.osuosl.org/pub/ubuntu/pool/multiverse/f/fdk-aac/libfdk-aac1_0.1.6-1_amd64.deb
+sudo dpkg -i libfdk-aac1_0.1.6-1_amd64.deb
+rm -rf libfdk-aac1_0.1.6-1_amd64.deb
+
+#obs-studio
+sudo apt install ffmpeg-y
+sudo add-apt-repository ppa:obsproject/obs-studio
+sudo apt update
+sudo apt install obs-studio
   
 #wireless drivers
 #apt install -fy build-essential libelf-dev linux-headers-`uname -r` bc dkms

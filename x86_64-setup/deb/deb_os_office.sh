65996408ac51fe4658c476b7de085aaf7b7511a9
gndpwnd <88689368+gndpwnd@users.noreply.github.com>
2021-12-15 16:06:34 -0900

    Update deb_os_office.sh

--- x86_64-setup/deb/deb_os_office.sh
+++ x86_64-setup/deb/deb_os_office.sh
@@ -1,55 +1,126 @@
 #!/bin/bash
 mkdir /opt/server
+cd /opt/
 
-#networking
-sudo echo "nameserver 1.1.1.1" > /etc/resolv.conf
-sudo echo "nameserver 1.0.0.1" >> /etc/resolv.conf
+if (( $EUID != 0 )); then
+  echo "This must be run as root. Type in 'sudo bash $0' to run it as root."
+  exit 1
+fi
 
-sudo apt update && sudo apt install -fy curl wget python3 python3-pip
+##################################################
+#                   Networking
+##################################################
+
+echo "nameserver 1.1.1.1" > /etc/resolv.conf
+echo "nameserver 1.0.0.1" >> /etc/resolv.conf
+
+##################################################
+#                   PG Lang
+##################################################
+
+# Rust
+
 curl https://sh.rustup.rs -sSf | sh
+export $PATH="$HOME/.cargo/bin:$PATH"
+
+# Nodejs
+
+curl -sL https://deb.nodesource.com/setup_14.x | sudo bash -
+sudo echo "deb-src https://deb.nodesource.com/node_14.x focal main" > /etc/apt/sources.list
+sudo apt -y install nodejs gcc g++ make
+node  -v
+
+# Yarn
+
+curl -sL https://dl.yarnpkg.com/debian/pubkey.gpg | sudo apt-key add -
+echo "deb https://dl.yarnpkg.com/debian/ stable main" | sudo tee /etc/apt/sources.list.d/yarn.list
+sudo apt update && sudo apt install yarn
+yarn -V
+
+# Platformio (C/C++ Framework)
+
 wget https://raw.githubusercontent.com/platformio/platformio-core-installer/master/get-platformio.py -O get-platformio.py
 python3 get-platformio.py
 rm -rf get-platformio.py
-sudo apt update && sudo apt upgrade -y
-sudo apt install -fy vim snap gparted build-essential libelf-dev linux-headers-`uname -r` bc dkms \ 
+
+
+##################################################
+#                APT Repo Packages
+##################################################
+
+apt update && apt upgrade -y
+apt install -fy vim snap gparted build-essential libelf-dev linux-headers-`uname -r` bc dkms \ 
                      apt-transport-https xclip terminator thunderbird libreoffice chromium-browser firefox \
                      libgconf-2-4 libappindicator1 libc++1 adb \
                      git docker.io iperf3 speedtest-cli golang \
-                     
-                     
-sudo snap install ao spotify vlc discord cherrytree cura-slicer rpi-imager
-sudo snap install blender --channel=2.79/stable --classic
-sudo snap install sublime-text --classic
-sudo snap install code --classic
- 
-sudo add-apt-repository ppa:maarten-baert/simplescreenrecorder
-sudo apt-get update
-sudo apt-get install simplescreenrecorder
+                     ffmpeg curl wget \
+
+apt --fix-broken install
+
+##################################################
+#                APT Standalone Packages
+##################################################
 
 wget http://ftp.osuosl.org/pub/ubuntu/pool/multiverse/f/fdk-aac/libfdk-aac1_0.1.6-1_amd64.deb
-sudo dpkg -i libfdk-aac1_0.1.6-1_amd64.deb
+dpkg -i libfdk-aac1_0.1.6-1_amd64.deb
 rm -rf libfdk-aac1_0.1.6-1_amd64.deb
+apt --fix-broken install
 
-#obs-studio
-sudo apt install ffmpeg-y
-sudo add-apt-repository ppa:obsproject/obs-studio
-sudo apt update
-sudo apt install obs-studio
-  
+##################################################
+#               Snap Packages
+##################################################
+
+snap install ao spotify vlc discord cherrytree cura-slicer rpi-imager
+snap install blender --channel=2.79/stable --classic
+snap install sublime-text --classic
+snap install code --classic
+
+##################################################
+#            PPA Repos and Packages
+##################################################
+
+add-apt-repository ppa:maarten-baert/simplescreenrecorder
+apt-get update
+apt-get install simplescreenrecorder
+
+add-apt-repository ppa:obsproject/obs-studio
+apt update
+apt install obs-studio
+
+apt --fix-broken install
+
+
+##################################################
+#          Alfa USB WiFi Adapter Drivers
+##################################################
+
 #wireless drivers
-#apt install -fy build-essential libelf-dev linux-headers-`uname -r` bc dkms
-#apt install -fy realtek-rtl88xxau-dkms
-cd /opt
 git clone https://github.com/aircrack-ng/rtl8812au.git
 cd rtl8812au/
-sudo make && sudo make install
+make && make install
 cd /opt/
 
-sudo apt --fix-broken install
+##################################################
+#                 PGLang Setup
+##################################################
+
+#pypi
+
+pip3 isntall numpy scipy matplotlib pandas nose tk \
+             argparse imutils tqdm \
+             jupyter jupyterlab  opencv-python
+
+
+#jupyter extensions
+
+jupyter labextension install jupyterlab-plotly
+
+##################################################
+#       Open Links To Get Latest Packages
+##################################################
 
 #Download
 x-www-browser https://www.realvnc.com/en/connect/download/viewer/
 x-www-browser https://obsidian.md/download
 
 chmod -R 777 /opt/
-

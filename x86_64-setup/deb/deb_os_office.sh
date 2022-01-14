85b7d7364fd799843aca8f4580db4fc5d8070a2c
gndpwnd <88689368+gndpwnd@users.noreply.github.com>
2022-01-13 16:39:26 -0900

    Update deb_os_office.sh

--- x86_64-setup/deb/deb_os_office.sh
+++ x86_64-setup/deb/deb_os_office.sh
@@ -28,7 +28,7 @@ blender_version="2.79"
 libfdk_aac_version="0.1.6-1"
 obsidian_version="0.12.15"
 VNC_Viewer_version="6.21.1109"
-go_version="1.14.2"
+go_version="1.17.6"
 
 ##################################################
 #                   Networking
@@ -36,37 +36,6 @@ go_version="1.14.2"
 
 echo "nameserver 1.1.1.1" > /etc/resolv.conf
 echo "nameserver 1.0.0.1" >> /etc/resolv.conf
-
-##################################################
-#                   PG Lang
-##################################################
-
-# Rust
-
-wget https://sh.rustup.rs -O rustup.sh && bash rustup.sh -y && rm -rf rustup.sh
-PATH="$HOME/.cargo/bin:$PATH"
-
-# Nodejs
-
-curl -sL https://deb.nodesource.com/setup_14.x | sudo bash -
-sudo echo "deb-src https://deb.nodesource.com/node_14.x focal main" > /etc/apt/sources.list
-sudo apt -y install nodejs gcc g++ make
-PATH="$HOME/.local/bin:$PATH"
-
-node  -v
-
-# Yarn
-
-curl -sL https://dl.yarnpkg.com/debian/pubkey.gpg | sudo apt-key add -
-echo "deb https://dl.yarnpkg.com/debian/ stable main" | sudo tee /etc/apt/sources.list.d/yarn.list
-sudo apt update && sudo apt install yarn
-yarn -V
-
-# Platformio (C/C++ Framework)
-
-wget https://raw.githubusercontent.com/platformio/platformio-core-installer/master/get-platformio.py -O get-platformio.py
-python3 get-platformio.py
-rm -rf get-platformio.py
 
 
 ##################################################
@@ -134,27 +103,5 @@ git clone https://github.com/aircrack-ng/rtl8812au.git
 cd rtl8812au/
 make && make install
 cd /opt/
-
-##################################################
-#                 PGLang Setup
-##################################################
-
-#pypi
-
-pip3 isntall numpy scipy matplotlib pandas nose tk \
-             argparse imutils tqdm rich \
-             jupyter jupyterlab \
-             opencv-python
-
-
-#jupyter extensions
-
-jupyter labextension install jupyterlab-plotly
-
-# golang
-
-wget -c https://dl.google.com/go/go${go_version}.linux-amd64.tar.gz -O - | sudo tar -xz -C /usr/local
-PATH="/usr/local/go/bin:$PATH"
-
 
 chmod -R 777 /opt/

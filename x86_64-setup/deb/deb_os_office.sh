bd2ffa5dd38b9d6a3d57065fb8ab376da977d46b
gndpwnd <88689368+gndpwnd@users.noreply.github.com>
2022-03-20 21:23:12 -0800

    Update deb_os_office.sh

--- x86_64-setup/deb/deb_os_office.sh
+++ x86_64-setup/deb/deb_os_office.sh
@@ -82,6 +82,10 @@ snap install code --classic
 ##################################################
 #            PPA Repos and Packages
 ##################################################
+apt install -fy apt-transport-https ca-certificates curl gnupg
+curl -fsSL https://gvisor.dev/archive.key | sudo gpg --dearmor -o /usr/share/keyrings/gvisor-archive-keyring.gpg
+echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/gvisor-archive-keyring.gpg] https://storage.googleapis.com/gvisor/releases release main" | sudo tee /etc/apt/sources.list.d/gvisor.list > /dev/null
+sudo apt-get update && sudo apt-get install -y runsc
 
 add-apt-repository ppa:maarten-baert/simplescreenrecorder
 apt-get update
